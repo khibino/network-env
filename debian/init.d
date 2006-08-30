b91104e54343b5f3ef6d29dc19ecec8aeeecb05b
hibi <none@none>
2006-08-30 06:43:29 +0000

    * Version 0.3.7 release.
    --HG--
    branch : hibino

--- debian/init.d
+++ debian/init.d
@@ -1,31 +1,32 @@
 #! /bin/sh
 PATH="/sbin:/usr/sbin:/bin:/usr/bin"
+set -e
 
 test -x /usr/sbin/nenv-change || exit 0
 
 case "$1" in
 	start)
 #		echo -n "Starting Mobile network environment: "
-#  		nenv-change `nenv-current`
+#  		nenv-change -q `nenv-current`
 #		echo "done."
   		;;
 	stop)
 		echo "Stopping mobile network environment: "
 		if [ x`nenv-current` != "xstand-alone" ]; then 
-		    nenv-change stand-alone
+		    nenv-change -q stand-alone
 		fi
 		echo "done."
   		;;
 	reload)
 		echo "Reloading mobile network environment: "
-		nenv-change `nenv-current`
+		nenv-change -q `nenv-current`
 		echo "done."
   		;;
 	restart|force-reload)
 		echo "Restarting mobile network environment: "
 		$0 stop
 		sleep 2
-		nenv-change `nenv-previous`
+		nenv-change -q `nenv-previous`
 		echo "done."
   		;;
 	*)
